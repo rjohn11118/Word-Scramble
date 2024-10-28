4e4ae4d204225dced75c2a94493068fd05fa0843
River <river.johnson@stu.barren.kyschools.us>
2024-10-28 08:14:42 -0500

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Student on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
